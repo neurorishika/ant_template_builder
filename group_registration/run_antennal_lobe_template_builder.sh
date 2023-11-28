616c7fdd00f010167453d413d08fa6f95d719457
Rishika Mohanta <neurorishika@gmail.com>
2023-11-28 12:26:34 -0500

    Modified code for verbose output

--- group_registration/run_antennal_lobe_template_builder.sh
+++ group_registration/run_antennal_lobe_template_builder.sh
@@ -34,16 +34,28 @@ mkdir obiroi_al_$DATE/affine
 
 mkdir obiroi_al_$DATE/syn
 
+# let the user know that the directory structure has been created
+echo "Directory structure created"
+
 ## STEP 2: Copy Data to the directory
 
 # Copy the data from subdirectory to the current directory ./
 
 cp $DATA_DIRECTORY/$ID ./
 
+# let the user know that the data has been copied
+echo "Data copied to the current directory"
+
 ## STEP 3: Run the registration
 
+# Let the user know that the registration is starting
+echo "Affine registration starting"
+
 # Run the initial affine registration
-./ANTs/Scripts/buildtemplateparallel.sh -d 3 -i $ITERATIONS_AFFINE -m 1x0x0 -t RA -s CC -c 2 -j $THREADS_AFFINE -o affine_ $ID > stdout-affine-template.txt 2>stderr-affine-template.txt
+./ANTs/Scripts/buildtemplateparallel.sh -d 3 -i $ITERATIONS_AFFINE -m 1x0x0 -t RA -s CC -c 2 -j $THREADS_AFFINE -o affine_ $ID > >(tee -a stdout-affine-template.txt) 2> >(tee -a stderr-affine-template.txt >&2)
+
+# let the user know that the affine registration has been completed
+echo "Affine registration completed"
 
 # Move all generated files to the affine subdirectory
 # Things to move: affine_* stdout-affine-template.txt, stderr-affine-template.txt, *.cfg, job*.* and GR* folders
@@ -54,18 +66,32 @@ mv stderr-affine-template.txt obiroi_al_$DATE/affine
 mv *.cfg obiroi_al_$DATE/affine
 mv RA* obiroi_al_$DATE/affine
 
+# let the user know that the affine registration has been completed
+echo "Affine registration files moved to the affine subdirectory"
+
 # Copy the affine template from the affine registration to the current directory ./ and run directory
 cp obiroi_al_$DATE/affine/affine_template.nii.gz ./affine_template.nii.gz
 cp obiroi_al_$DATE/affine/affine_template.nii.gz obiroi_al_$DATE/affine_template.nii.gz
 
-# check if there is a diff folder in the resampled_data directory
+# let the user know that the affine template has been copied
+echo "Affine template copied to the current directory"
+
+# check if there is a diff folder in the resampled_data directory (ARCHIVED: NO LONGER USED BUT KEPT FOR LEGACY PURPOSES)
 if [ -d "../resampled_data/diff" ]; then
     # Copy the diff data from ../resampled_data/diff to the current directory ./
     cp ../resampled_data/diff/$ID ./
+    # let the user know that the diff data has been copied
+    echo "Diff data copied to the current directory"
 fi
 
+# Let the user know that the syn registration is starting
+echo "Syn registration starting"
+
 # Run the syn registration
-./ANTs/Scripts/buildtemplateparallel.sh -d 3 -i $ITERATIONS_SYN -m 30x90x20x8 -t GR -s CC -z affine_template.nii.gz -c 2 -j $THREADS_SYN -o complete_  $ID > stdout-syn-template.txt 2>stderr-syn-template.txt
+./ANTs/Scripts/buildtemplateparallel.sh -d 3 -i $ITERATIONS_SYN -m 30x90x20x8 -t GR -s CC -z affine_template.nii.gz -c 2 -j $THREADS_SYN -o complete_  $ID > >(tee -a stdout-syn-template.txt) 2> >(tee -a stderr-syn-template.txt >&2)
+
+# let the user know that the syn registration has been completed
+echo "Syn registration completed"
 
 # Move all generated files to the syn subdirectory
 # Things to move: complete_* stdout-syn-template.txt, stderr-syn-template.txt, *.cfg, job*.* and GR* folders
@@ -76,16 +102,31 @@ mv stderr-syn-template.txt obiroi_al_$DATE/syn
 mv *.cfg obiroi_al_$DATE/syn
 mv GR* obiroi_al_$DATE/syn
 
-# delete all temporary folders (starting with temp)
-rm -rf temp*
+# let the user know that the syn registration has been completed
+echo "Syn registration files moved to the syn subdirectory"
 
 # Copy the syn template from the syn registration to the run directory
 cp obiroi_al_$DATE/syn/complete_template.nii.gz obiroi_al_$DATE/complete_template.nii.gz
+
+# let the user know that the syn template has been copied
+echo "Syn template copied to the current directory"
 
 # Delete the intermediate files
 rm affine_template.nii.gz
 rm *.nii.gz
 rm *.nrrd
+rm -rf temp*
+
+# let the user know that the intermediate files have been deleted
+echo "Intermediate files deleted"
 
 # Move the run directory to the results directory
 mv obiroi_al_$DATE ../results
+
+# let the user know that the run directory has been moved to the results directory
+echo "Run directory moved to the results directory"
+
+# Let the user know that the template building is complete
+echo "Template building complete"
+
+# End of script
